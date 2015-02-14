681e94a339cb5a7faed0cb03257e8cb979371305
xinyu198736 <xinyu198736@gmail.com>
2015-02-14 23:07:24 +0800

    Initial Commit

---
+++ sleepstory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sleepstory
+//
+//  Created by tianqi on 15/2/14.
+//  Copyright (c) 2015年 html-js. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
